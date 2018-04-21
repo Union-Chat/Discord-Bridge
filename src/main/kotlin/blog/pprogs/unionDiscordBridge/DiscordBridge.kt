f998cc06f9aba8b09a921252d7f4924e89278c5f
Perry Fraser <perry@frasers.org>
2018-04-21 19:13:34 -0400

    two way bridge

--- src/main/kotlin/blog/pprogs/unionDiscordBridge/DiscordBridge.kt
+++ src/main/kotlin/blog/pprogs/unionDiscordBridge/DiscordBridge.kt
@@ -3,13 +3,17 @@ package blog.pprogs.unionDiscordBridge
 import blog.pprogs.ktunion.UnionClient
 import net.dv8tion.jda.core.AccountType
 import net.dv8tion.jda.core.JDABuilder
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent
+import net.dv8tion.jda.core.hooks.ListenerAdapter
 import java.io.File
+import kotlin.concurrent.thread
 
 fun main(args: Array<String>) {
     val creds = File("creds.txt").readLines()
-    val client = UnionClient(username = creds[0], password = creds[1], selfbot = true, silent = true)
+    val client = UnionClient(username = creds[0], password = creds[1], selfbot = true)
     val jda = JDABuilder(AccountType.BOT)
             .setToken(creds[2])
+            .addEventListener(Listener(client))
             .buildAsync()
 
     client.onTextMessage = { who, content, _ ->
@@ -18,3 +22,15 @@ fun main(args: Array<String>) {
     client.start()
 }
 
+class Listener(private val client: UnionClient) : ListenerAdapter() {
+    override fun onMessageReceived(event: MessageReceivedEvent?) {
+        if (event == null) return
+
+        if (event.author.idLong == 429038614320513024) return
+
+        if (event.channel.idLong != 429044006392037376) return
+
+        client.sendMessage("<${event.author.name}> ${event.message.contentRaw}")
+        event.message.delete().queue()
+    }
+}
